6d3f76c2748f753b001b33d89b811038c33b6c8e
gordon <git@gordon.re>
2012-10-10 00:05:50 +0200

    Added Figlet plugin

---
+++ plugins/figlet.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from pyfiglet import Figlet
+from stdPlugin import stdPlugin
+
+class figlet(stdPlugin):
+
+    events = [('pubmsg', {'priority': 3, 'exclusive': True, 'command_namespace': 'figlet'})]
+
+    def __init__(self, bot, conf):
+        self.bot = bot
+        self.conf = conf
+
+    def on_cmd(self, serv, ev, command, args):
+        f = Figlet()
+        args = [command] + args
+        message = ' '.join(args)
+        for line in f.renderText(message).split('\n'):
+            serv.privmsg(ev.target(), line)

vegaelle/naobot

commit 4b9805eb2676c33b3b8b22cfff1049fba5784163

Learn: simplify parsing

plugins/learn.py
@@ -81,16 +81,12 @@ class learn(stdPlugin):
     def parse(self, chan, sentence):
         sentences = re.split(self.end_chars, sentence)
         for sentence in sentences:
-            current_word = 0
+            current_word = 1
             words = sentence.split()
-            last_word = len(words) - 1
-            for word in words:
-                if current_word == 0:
-                    self.add_successor_to_word(chan, self.begin_word, word)
-                else:
-                    self.add_successor_to_word(chan, words[current_word-1], word)
-                if current_word == last_word:
-                    self.add_successor_to_word(chan, word, self.end_word)
+            words.insert(0, self.begin_word)
+            words.append(self.end_word)
+            for word in words[1:]:
+                self.add_successor_to_word(chan, words[current_word-1], word)
                 current_word += 1
         self.save_dico(chan)
 

commit ae7e52b94bb9a571038b48ce2fbddbd23abb2a7c

Learn: Fix empty sentence bug introduced in 4b9805eb

plugins/learn.py
@@ -79,7 +79,8 @@ class learn(stdPlugin):
         return self.bot.write_config(self, chan, self.dico[chan])
 
     def parse(self, chan, sentence):
-        sentences = re.split(self.end_chars, sentence)
+        # We don't want to keep empty sentences
+        sentences = [s for s in re.split(self.end_chars, sentence) if s]
         for sentence in sentences:
             current_word = 1
             words = sentence.split()
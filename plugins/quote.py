1f517f0175db0caf1e9a7893d332d23f42447c1e
Damien Nicolas <damien@gordon.re>
2013-02-13 11:07:32 +0100

    Fixed crash in quote plugin

--- plugins/quote.py
+++ plugins/quote.py
@@ -68,26 +68,29 @@ class quote(stdPlugin):
         %(namespace)s : sélectionne une phrase de la liste.
         %(namespace)s #<id> : sélectionne la phrase d’un id donné.
         %(namespace)s ?<mot-clé> : sélectionne une phrase contenant le mot-clé'''
-        if not command:
-            quote = self.get_random_quote(helper['target'])
-            return self.say_quote(serv, helper['target'], quote)
-        elif command.startswith('#'):
-            quote = self.get_quote(helper['target'], command[1:])
-            return self.say_quote(serv, helper['target'], quote)
-        elif command.startswith('?'):
-            args.insert(0, command)
-            keyword = ' '.join(args)[1:]
-            quotes = self.search_quote(helper['target'], keyword)
-            if quotes:
-                quote = random.choice(quotes)
+        try:
+            if not command:
+                quote = self.get_random_quote(helper['target'])
                 return self.say_quote(serv, helper['target'], quote)
+            elif command.startswith('#'):
+                quote = self.get_quote(helper['target'], command[1:])
+                return self.say_quote(serv, helper['target'], quote)
+            elif command.startswith('?'):
+                args.insert(0, command)
+                keyword = ' '.join(args)[1:]
+                quotes = self.search_quote(helper['target'], keyword)
+                if quotes:
+                    quote = random.choice(quotes)
+                    return self.say_quote(serv, helper['target'], quote)
+                else:
+                    return self.say_quote(serv, helper['target'], None)
             else:
-                return self.say_quote(serv, helper['target'], None)
-        else:
-            args.insert(0, command)
-            quote = ' '.join(args)
-            id = self.add_quote(helper['target'], quote, helper['sender'])
-            if id:
-                serv.privmsg(helper['target'], u'Quote enregistrée ! ID %d' % id)
-                return True
+                args.insert(0, command)
+                quote = ' '.join(args)
+                id = self.add_quote(helper['target'], quote, helper['sender'])
+                if id:
+                    serv.privmsg(helper['target'], u'Quote enregistrée ! ID %d' % id)
+                    return True
+        except:
+            serv.privmsg(helper['target'], u'Nope.')
         return False

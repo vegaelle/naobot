9bf91e9c9d7eb437327c99a143f2eb1311df2d5c
Damien Nicolas <damien@gordon.re>
2013-04-26 16:42:15 +0200

    Fixed bulgroz plugin

--- plugins/bulgroz.py
+++ plugins/bulgroz.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from stdPlugin import stdPlugin
 import re
+from random import random, choice
+from stdPlugin import stdPlugin
 
 class bulgroz(stdPlugin):
     u'''Implémente les plus originales phrases de spammeurs.'''
@@ -9,61 +10,59 @@ class bulgroz(stdPlugin):
     events = {'pubmsg': {'exclusive': True},
               'action': {'exclusive': True}}
 
-    paypal_messages = ["C'EST OUI CONFIRMANT",
-            "VOUS ÊTES LE 10.000.00 COMPTE PAYPAL",
-            "OFFRE DU GRATUITE POUR PAIEMENT!",
-            "SVP CLIQUEZ LA CONFIRMATION POUR UN OUI",
-            "C'EST LE MESSAGE 10.00.0.000 SUR LE RÉSEAU DU CHAT SOCIALE (IRC)",
-            "POSSIBLE OBTENIR BITCOINS AVEC PARTENARIAT AMD/ATI",
-            "INSCRIVEZ-VOUS AVEC COURRIER D'ADRESSE POUR MEILLEUR DE L'OFFRE",
-            "ABLATION DU POLYPE AVEC PINCES DE CRUSTACÉ, PRÉVOYEZ PAYPAL "
-            "SI ANESTHÉSIE",
-            "SI PRIX IMPOSSIBLE DU FRAIS DE DOUANE CLIQUEZ LA CONNEXION "
-            "POUR LIVRAISON SUR VPN",
-            "C'EST OFFREMENT POUR MOINS CHER AVEC CODE 1337 SUR BOUTIQUE "
-            "PRÉVOYEZ PAYPAL",
-            "PRÉVOIR CARTE DE BANQUE POUR INSCRIVAGE EN CONNEXION DES "
-            "RENCONTRES!!"]
+    paypal_messages = [u"C'EST OUI CONFIRMANT",
+            u"VOUS ÊTES LE 10.000.00 COMPTE PAYPAL",
+            u"OFFRE DU GRATUITE POUR PAIEMENT!",
+            u"SVP CLIQUEZ LA CONFIRMATION POUR UN OUI",
+            u"C'EST LE MESSAGE 10.00.0.000 SUR LE RÉSEAU DU CHAT SOCIALE (IRC)",
+            u"POSSIBLE OBTENIR BITCOINS AVEC PARTENARIAT AMD/ATI",
+            u"INSCRIVEZ-VOUS AVEC COURRIER D'ADRESSE POUR MEILLEUR DE L'OFFRE",
+            u"ABLATION DU POLYPE AVEC PINCES DE CRUSTACÉ, PRÉVOYEZ PAYPAL "
+            u"SI ANESTHÉSIE",
+            u"SI PRIX IMPOSSIBLE DU FRAIS DE DOUANE CLIQUEZ LA CONNEXION "
+            u"POUR LIVRAISON SUR VPN",
+            u"C'EST OFFREMENT POUR MOINS CHER AVEC CODE 1337 SUR BOUTIQUE "
+            u"PRÉVOYEZ PAYPAL",
+            u"PRÉVOIR CARTE DE BANQUE POUR INSCRIVAGE EN CONNEXION DES "
+            u"RENCONTRES!!"]
 
-    cadeau_messages = ["OBTENEZ OFFRE DE MOINS CHER POUR %s EN CLIQUANT "
-            "LE PAYPAL",
-            "%s SVP ?? ICI C'EST RÉDUCTIONS DES SOLDES SUR TOUTE LA "
-            "SEMAINE POUR LE %s",
-            "%s EXPÉDIÉ GRATUITEMENT SI COMMANDE DE LOT EN QUANTITÉ "
-            "GRANDE POUR BEAUCOUP",
-            "IL DÉSTOCKE %s SUR LA RÉGION DE VOTRE NRA OUVERT MÊME "
-            "DIMANCHE NUIT ET SHABBAT",
-            "C'EST GRAND BESOIN DE %s ???? SANS ATTENTE DEMANDEZ LIVRAISON "
-            "POUR HIER ET RECEVEZ MAINTENANT!! TRÈS RAPIDE SI PAYPAL!!",
-            "VOUS SAVEZ AVOIR BESOIN DE %s??? DONC CLIQUE VITE L'OFFRE SUR "
-            "L'ÉCRAN ET REÇOIT %s CHEZ TOI DANS VOTRE MAISON!",
-            "LA PROMOTION EST CHANCE DE VOUS POUR %s DU DISPONIBILITÉ "
-            "INSTANTANÉE!!"]
+    cadeau_messages = [u"OBTENEZ OFFRE DE MOINS CHER POUR %s EN CLIQUANT "
+            u"LE PAYPAL",
+            u"%s SVP ?? ICI C'EST RÉDUCTIONS DES SOLDES SUR TOUTE LA "
+            u"SEMAINE POUR LE %s",
+            u"%s EXPÉDIÉ GRATUITEMENT SI COMMANDE DE LOT EN QUANTITÉ "
+            u"GRANDE POUR BEAUCOUP",
+            u"IL DÉSTOCKE %s SUR LA RÉGION DE VOTRE NRA OUVERT MÊME "
+            u"DIMANCHE NUIT ET SHABBAT",
+            u"C'EST GRAND BESOIN DE %s ???? SANS ATTENTE DEMANDEZ LIVRAISON "
+            u"POUR HIER ET RECEVEZ MAINTENANT!! TRÈS RAPIDE SI PAYPAL!!",
+            u"VOUS SAVEZ AVOIR BESOIN DE %s??? DONC CLIQUE VITE L'OFFRE SUR "
+            u"L'ÉCRAN ET REÇOIT %s CHEZ TOI DANS VOTRE MAISON!",
+            u"LA PROMOTION EST CHANCE DE VOUS POUR %s DU DISPONIBILITÉ "
+            u"INSTANTANÉE!!"]
 
-    bortz_messages = ["BORTZ RULEZ !",
-                      "JOIN BORT'Z FANCLUB !", 
-                      "CLIQUEZ LE PAYPAL POUR ABONNEMENT GRATUIT À BLOGZMEYER SUR HTTP://WWW.BORTZMEYER.ORG !",
-                      "LES RFC SONT EN ILLIMITÉ SUR BLOGZMEYER POUR ADMINISTREMENT DES INTERNETZ !"]
+    bortz_messages = [u"BORTZ RULEZ !",
+                      u"JOIN BORT'Z FANCLUB !", 
+                      u"CLIQUEZ LE PAYPAL POUR ABONNEMENT GRATUIT À BLOGZMEYER SUR HTTP://WWW.BORTZMEYER.ORG !",
+                      u"LES RFC SONT EN ILLIMITÉ SUR BLOGZMEYER POUR ADMINISTREMENT DES INTERNETZ !"]
 
-    pedo = ("PÉDO ?? CLIQUEZ POUR ICI ET DÃ‰NONCER ACTION ILLÉGALE DE "
-            "ENFANT, CLIQUEZ POUR LÀ ET OBTENIR SEXTAPE DE LA RUSSIE")
+    pedo = (u"PÉDO ?? CLIQUEZ POUR ICI ET DÃ‰NONCER ACTION ILLÉGALE DE "
+            u"ENFANT, CLIQUEZ POUR LÀ ET OBTENIR SEXTAPE DE LA RUSSIE")
     single_answer = {
-            "coin": "pan",
-            "nioc": "nap",
-            "tchouu": "VOYAGEZ TOUTE LA SAISON SUR LE CHEMINEMENT DE FER "
-            "GRÂCE À WEB DE BOUTIQUE POUR LES BILLETS DU WAGON",
-            "zoidberg": "PROFITEZ DE PARTENARIAT AVEC DOCTEUR DU "
-            "MOLLUSQUE ET OBTENEZ SANTÉ POUR VOUS & VOS ENFANTS & TOUTE "
-            "FAMILLE (MÊME HOMO)",
-            "pédo": pedo,
-            "pedo": pedo,
+            u"tchouu": "VOYAGEZ TOUTE LA SAISON SUR LE CHEMINEMENT DE FER "
+            u"GRÂCE À WEB DE BOUTIQUE POUR LES BILLETS DU WAGON",
+            u"zoidberg": "PROFITEZ DE PARTENARIAT AVEC DOCTEUR DU "
+            u"MOLLUSQUE ET OBTENEZ SANTÉ POUR VOUS & VOS ENFANTS & TOUTE "
+            u"FAMILLE (MÊME HOMO)",
+            u"pédo": pedo,
+            u"pedo": pedo,
     }
 
     def on_pubmsg(self, serv, ev, helper):
         message = helper['message']
         long_words = re.findall(r"\b\w{8,}\b", message)
 
-        for word in single_answer:
+        for word in self.single_answer:
             if re.search(r"\b%s\b" % re.escape(word), message, re.I):
                 serv.privmsg(helper['target'], self.single_answer[word])
                 return True

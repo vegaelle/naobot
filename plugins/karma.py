aa1c8b4819e4a3854bf574e490079273fd2129a9
Baptiste Jonglez <baptiste--git@jonglez.org>
2013-05-07 20:09:40 +0200

    Karma : indicates a preference for a word when somebody uses it

--- plugins/karma.py
+++ plugins/karma.py
@@ -12,8 +12,11 @@ class karma(stdPlugin):
               'action': {'exclusive': False},
              }
 
-    regexp_pos = re.compile(r"([\w\-_/]+)\+\+", re.U)
-    regexp_neg = re.compile(r"([\w\-_/]+)--", re.U)
+    # A bit complicated, since we allow "-" but not "--" inside a word.
+    regexp_pos = re.compile(r"(([\w\_'/]-?)+)\+\+", re.U)
+    regexp_neg = re.compile(r"(([\w\_'/]-?)+)--", re.U)
+
+    punctuation = u",.  !?"
     
     # chan -> (word -> karma)
     # karma is an integer
@@ -25,6 +28,17 @@ class karma(stdPlugin):
         for chan in chans:
             self.load_karmadb(chan)
         return return_val
+
+    def find_all_karma(self, message, plus):
+        """Returns the list of words that were used """
+        if plus:
+            r = self.regexp_pos
+        else:
+            r = self.regexp_neg
+        # In the regexp, we must use a second pair of parentheses to
+        # group things, which causes re.findall() to output a list of
+        # couples. Quick fix.
+        return [x[0] for x in r.findall(message)]
 
     def get_karma(self, chan, word):
         word = word.lower()
@@ -49,28 +63,42 @@ class karma(stdPlugin):
         self.modify_karma(chan, word, -1)
 
 
-    def parse(self, chan, message):
+    def parse_karma(self, chan, message):
         changed = False
-        for word in self.regexp_pos.findall(message):
+        for word in self.find_all_karma(message, plus=True):
             self.increase_karma(chan, word)
             changed = True
-        for word in self.regexp_neg.findall(message):
+        for word in self.find_all_karma(message, plus=False):
             self.decrease_karma(chan, word)
             changed = True
         if changed:
             self.save_karmadb(chan)
 
+    def match_words(self, serv, chan, message):
+        """Try to match a word known in the karma database, and react accordingly."""
+        answered = False
+        words = [w.strip(self.punctuation) for w in message.split()]
+        for w in words:
+            k = self.get_karma(chan, w)
+            if k > 0:
+                serv.privmsg(chan, u"Yeah, j'adore %s \o/" % w)
+                answered = True
+            if k < 0:
+                serv.privmsg(chan, u"C'est nul, %s" % w)
+                answered = True
+        return answered
+
     def on_pubmsg(self, serv, ev, helper):
-        self.parse(helper['target'], helper['message'])
-        return False
+        self.parse_karma(helper['target'], helper['message'])
+        return self.match_words(serv, helper['target'], helper['message'])
 
     def on_privmsg(self, serv, ev, helper):
-        self.parse(helper['target'], helper['message'])
-        return False
+        self.parse_karma(helper['target'], helper['message'])
+        return self.match_words(serv, helper['target'], helper['message'])
 
     def on_action(self, serv, ev, helper):
-        self.parse(helper['target'], helper['message'])
-        return False
+        self.parse_karma(helper['target'], helper['message'])
+        return self.match_words(serv, helper['target'], helper['message'])
 
     def on_join(self, serv, ev, helper):
         if helper['sender'] == serv.username: #s’il s’agit de notre propre join

922499f8dfe8147f9a106f86600223b2a2b99521
Damien Nicolas <damien@gordon.re>
2013-07-17 16:45:19 +0200

    Added nsa plugin

---
+++ plugins/nsa.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import random
+import os
+from stdPlugin import stdPlugin, PluginError
+
+class nsa(stdPlugin):
+    u'''Attire l’attention de la National Security Agency sur le chan.'''
+
+    events = {
+            'run': {'frequency': (30, 60)},
+        }
+
+    def __init__(self, bot, conf):
+        return_val = super(nsa, self).__init__(bot, conf)
+        try:
+            file = open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'nsa')).read()
+            self.words = file.replace('\n', ' ').split(', ')
+        except Exception, e:
+            raise PluginError('No words found: %s.' % e)
+        import ipdb; ipdb.set_trace()
+        return return_val
+
+    def gen_sentence(self):
+        sentence = []
+        for i in xrange(random.randint(5, 15)):
+            sentence.append(random.choice(self.words))
+        return ' '.join(sentence)
+
+    def on_run(self, serv, helper):
+        serv.privmsg(helper['target'], self.gen_sentence())
+
+
+

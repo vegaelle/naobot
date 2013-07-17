57055c9447ef94dbac5bb60b6708dcd170dfe4b0
Damien Nicolas <damien@gordon.re>
2013-07-17 16:46:50 +0200

    Fixed nsa frequency

--- plugins/nsa.py
+++ plugins/nsa.py
@@ -8,7 +8,7 @@ class nsa(stdPlugin):
     u'''Attire l’attention de la National Security Agency sur le chan.'''
 
     events = {
-            'run': {'frequency': (30, 60)},
+            'run': {'frequency': (300, 3000)},
         }
 
     def __init__(self, bot, conf):

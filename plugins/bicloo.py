f6c5cfe8de73b10cdc78c2749f3311eed5d79f7a
Damien Nicolas <damien@gordon.re>
2016-11-23 12:45:18 +0100

    Migrate bicloo plugin (may not work)

--- plugins/bicloo.py
+++ plugins/bicloo.py
@@ -1,37 +1,43 @@
 # -*- coding: utf-8 -*-
 
-from stdPlugin import stdPlugin, PluginError
 from xml.etree import ElementTree
+
 import requests
 
+from irc3.plugins.command import command
+import irc3
 
-class bicloo(stdPlugin):
+
+@irc3.plugin
+class Bicloo:
     u'''Informe sur l’état du réseau de vélos Bicloo de Nantes'''
-
-    events = {'pubmsg': {'exclusive': True, 'command_namespace': 'bicloo'},
-              'privmsg': {'exclusive': True, 'command_namespace': 'bicloo'}}
 
     general_url = 'http://www.bicloo.nantesmetropole.fr/service/carto'
     station_url = \
         'http://www.bicloo.nantesmetropole.fr/service/stationdetails/nantes/%d'
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, bot):
+        self.bot = bot
         self.stations = {}
         self.stations_ids = {}
-        r = requests.get(self.general_url)
+        self.headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X'
+                        ' 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko)'
+                        'Chrome/39.0.2171.95 Safari/537.36'}
+
+        r = requests.get(self.general_url, headers=self.headers)
         if r.status_code is not 200:
-            raise PluginError('Unable to access Bicloo API.')
+            self.bot.log.error('Unable to access Bicloo API.')
+            return
         tree = ElementTree.fromstring(r.content)
         for marker in tree.find('markers').getchildren():
             name = marker.attrib['name'][6:].lower()
             self.stations[int(marker.attrib['number'])] = name
             self.stations_ids[name] = int(marker.attrib['number'])
-        return super(bicloo, self).__init__(*args, **kwargs)
 
-    def get_status(self, id):
+    def get_status(self, station_id):
 
-        url = self.station_url % int(id)
-        r = requests.get(url)
+        url = self.station_url % int(station_id)
+        r = requests.get(url, headers=self.headers)
         if r.status_code is not 200:
             return 'Numéro de station invalide'
         tree = ElementTree.fromstring(r.content)
@@ -41,22 +47,25 @@ class bicloo(stdPlugin):
         if station['open'] == '0' or station['connected'] == '0':
             return 'Station indisponible'
         return u'Station %s (%d) : %d vélos sur %d disponibles' %\
-            (self.stations[int(id)].title(), int(id),
+            (self.stations[int(station_id)].title(), int(station_id),
              int(station['available']), int(station['total']))
 
-    def on_cmd(self, serv, ev, command, args, helper):
-        u'''%(namespace)s <ID> : Indique le statut de la station <ID>
-        %(namespace)s <nom> : Indique le statut de la station <nom>'''
+    @command(permission='view')
+    def bicloo(self, mask, target, args):
+        """Bicloo
+        Indique le nombre de vélos disponibles sur une station
+
+            %%bicloo <station>...
+        """
+        cmd = ' '.join(args['<station>'])
         try:
-            if command.isdigit():
-                station_status = self.get_status(command)
-                serv.privmsg(helper['target'], station_status)
+            if cmd.isdigit():
+                station_status = self.get_status(cmd)
+                self.bot.privmsg(target, station_status)
             else:
-                args.insert(0, command)
-                station_name = ' '.join(args)
+                station_name = cmd
                 station_id = self.stations_ids[station_name.lower()]
                 station_status = self.get_status(station_id)
-                serv.privmsg(helper['target'], station_status)
+                yield station_status
         except:
-            serv.privmsg(helper['target'], 'Station inexistante')
-        return True
+            yield 'Station inexistante'
